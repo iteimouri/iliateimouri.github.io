37f4e621b6a05fc5c8bb4447a43831a7e688d99a
iteimouri <ilia.teimouri@gmail.com>
2024-04-30 02:04:38 +0100

    test1

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/iteimouri.github.io/',  // Change <repository-name> to your GitHub repository name or '/' for a user/organization page
-  build: {
-    outDir: 'dist'  // Vite defaults to 'dist', but it's good to be explicit
-  }
-});
